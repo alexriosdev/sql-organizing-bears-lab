56575af4f2eb11611e75891796846b969dee6ab1
Alex Rios <alexrios.dev@gmail.com>
2020-10-15 16:03:59 -0500

    Done.

---
+++ lib/insert.sql
@@ -0,0 +1,9 @@
+INSERT INTO bears (id, name, age, gender, color, temperament, alive) VALUES 
+(null, "Mr. Chocolate", null, "M", null, null, null),
+(null, "Rowdy", null, "M", null, null, null),
+(null, "Tabitha", null, "F", null, null, null),
+(null, "Sergeant Brown", null, "M", null, null, null),
+(null, "Melissa", null, "F", null, null, null),
+(null, "Grinch", null, "M", null, null, null),
+(null, "Wendy", null, "F", null, null, null),
+(null, null, null, "F", null, null, null);
